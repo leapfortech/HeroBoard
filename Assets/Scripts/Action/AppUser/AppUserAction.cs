15add4624cb56ccff149c2272ff591b5184f8334
leapfortech <46363814+miralbes@users.noreply.github.com>
2026-04-23 16:34:22 -0600

    UPD Display Users

--- Assets/Scripts/Action/AppUser/AppUserAction.cs
+++ Assets/Scripts/Action/AppUser/AppUserAction.cs
@@ -29,6 +29,8 @@ public class AppUserAction : MonoBehaviour
     [Title("Sprites")]
     [SerializeField]
     Sprite sprEmpty = null;
+    [SerializeField]
+    Sprite sprOnboarded = null;
 
     [Title("Data")]
     [SerializeField]
@@ -37,7 +39,7 @@ public class AppUserAction : MonoBehaviour
     public bool Selected { get; set; } = false;
 
     IdentityService identityService = null;
-    //AppUserService appUserService = null;
+    AppUserService appUserService = null;
     List<IdentityFull> identityFulls = null;
 
     IdentityFull identityFull = null;
@@ -45,12 +47,13 @@ public class AppUserAction : MonoBehaviour
     private void Awake()
     {
         identityService = GetComponent<IdentityService>();
-        //appUserService = GetComponent<AppUserService>();
+        appUserService = GetComponent<AppUserService>();
     }
 
     public void Clear()
     {
         //StateManager.Instance.IdentityFulls = new List<IdentityFull>();
+        identityFulls = new List<IdentityFull>();
         dtmIdentityFull.ClearElements();
     }
 
@@ -59,6 +62,7 @@ public class AppUserAction : MonoBehaviour
         ScreenDialog.Instance.Display();
 
         //StateManager.Instance.IdentityFulls = new List<IdentityFull>();
+        identityFulls = new List<IdentityFull>();
         lstAppUsers.ApplyClearValues();
         txtAppUsersEmpty.gameObject.SetActive(false);
 
@@ -68,117 +72,89 @@ public class AppUserAction : MonoBehaviour
 
     public void FillIdentitys(List<IdentityFull> identityFulls)
     {
-        this.identityFulls = identityFulls;
         //StateManager.Instance.IdentityFulls = identityFulls;
+        this.identityFulls = identityFulls;
 
-        //GetAppUsers();
+        GetAppUsers();
+    }
 
+    public void GetAppUsers()
+    {
+        appUserService.GetFullsByStatus(1);
+    }
+
+    public void FillAppUsers(List<AppUserFull> appUserFulls)
+    {
+        //if (StateManager.Instance.IdentityFulls == null)
         if (identityFulls == null)
         {
             lstAppUsers.ApplyClearValues();
             txtAppUsersEmpty.gameObject.SetActive(false);
+            identityFull = null;
+            //StateManager.Instance.IdentityFulls = new List<IdentityFull>(appUserFulls.Count);
+            identityFulls = new List<IdentityFull>(appUserFulls.Count);
+        }
+
+        AppUserFull a;
+        for (int i = 0; i < appUserFulls.Count; i++)
+        {
+            a = appUserFulls[i];
+
+            //StateManager.Instance.IdentityFulls.Add(new IdentityFull(-1, "-", null, "-", null, "-", DateTime.Now, "-", "-",
+            //                                                         a.PhonePrefix, a.Phone, a.Email, a.CreateDateTime,
+            //                                                         a.UpdateDateTime, a.AppUserStatusId, 0));
+
+            identityFulls.Add(new IdentityFull(-1, "-", null, "-", null, "-", DateTime.Now, "-", "-", "-",
+                                               a.PhonePrefix, a.Phone, a.Email, a.CreateDateTime,
+                                               a.UpdateDateTime, a.AppUserStatusId, 0));
+        }
+
+        //if (StateManager.Instance.IdentityFulls.Count == 0)
+        if (identityFulls.Count == 0)
+        {
+            lstAppUsers.ApplyClearValues();
+            txtAppUsersEmpty.gameObject.SetActive(true);
             StateManager.Instance.BoardLoadHide();
             return;
-            //identityFull = null;
-            //StateManager.Instance.IdentityFulls = new List<IdentityFull>(appUserFulls.Count);
         }
+
+        //StateManager.Instance.IdentityFulls.Sort((idf1, idf2) => { return idf1.Id.CompareTo(idf2.Id); });
+        identityFulls.Sort((idf1, idf2) => { return idf1.Id.CompareTo(idf2.Id); });
 
         lstAppUsers.ClearValues();
 
         ListScrollerValue lstAppUserValue;
+        //for (int i = 0; i < StateManager.Instance.IdentityFulls.Count; i++)
         for (int i = 0; i < identityFulls.Count; i++)
         {
             lstAppUserValue = new ListScrollerValue(4, true);
+            //IdentityFull identityFull = StateManager.Instance.IdentityFulls[i];
             IdentityFull identityFull = identityFulls[i];
 
-            lstAppUserValue.SetText(0, "");
-            lstAppUserValue.SetText(1, $"{identityFull.Email}");
-            lstAppUserValue.SetSprite(2, sprEmpty);
-            lstAppUserValue.SetSprite(3, sprEmpty);
+            if (identityFull.Status == 0)
+            {
+                lstAppUserValue.SetText(0, identityFull.PhonePrefix + " " + identityFull.Phone);
+                lstAppUserValue.SetText(1, $"{identityFull.Email}");
+                lstAppUserValue.SetSprite(2, sprEmpty);
+                lstAppUserValue.SetSprite(3, sprEmpty);
+            }
+            else
+            {
+                lstAppUserValue.SetText(0, identityFull.BirthCountry);
+                lstAppUserValue.SetText(1, $"{identityFull.FirstNames} {identityFull.LastNames}");
+                lstAppUserValue.SetSprite(2, sprOnboarded);
+                lstAppUserValue.SetSprite(3, sprOnboarded); 
+            }
 
             lstAppUsers.AddValue(lstAppUserValue);
         }
 
         lstAppUsers.ApplyValues();
 
-        if (identityFulls != null && identityFulls.Count != 0)
-            Display(0);
+        Display(0);
 
         StateManager.Instance.BoardLoadHide();
     }
-
-    //public void GetAppUsers()
-    //{
-    //    appUserService.GetFullsByStatus(1);
-    //}
-
-    //public void FillAppUsers(List<AppUserFull> appUserFulls)
-    //{
-    //    if (StateManager.Instance.IdentityFulls == null)
-    //    {
-    //        lstAppUsers.ApplyClearValues();
-    //        txtAppUsersEmpty.gameObject.SetActive(false);
-    //        identityFull = null;
-    //        StateManager.Instance.IdentityFulls = new List<IdentityFull>(appUserFulls.Count);
-    //    }
-
-    //    AppUserFull a;
-    //    for (int i = 0; i < appUserFulls.Count; i++)
-    //    {
-    //        a = appUserFulls[i];
-    //        StateManager.Instance.IdentityFulls.Add(new IdentityFull(-1, "-", null, "-", null, "-", DateTime.Now, "-", "-", 
-    //                                                                 a.PhonePrefix, a.Phone, a.Email, a.CreateDateTime,
-    //                                                                 a.UpdateDateTime, a.AppUserStatusId, 0));
-    //    }
-
-    //    if (StateManager.Instance.IdentityFulls.Count == 0)
-    //    {
-    //        lstAppUsers.ApplyClearValues();
-    //        txtAppUsersEmpty.gameObject.SetActive(true);
-    //        StateManager.Instance.BoardLoadHide();
-    //        return;
-    //    }
-
-    //    //RM
-    //    StateManager.Instance.IdentityFulls.Sort((idf1, idf2) => { return idf1.Id.CompareTo(idf2.Id); });
-
-    //    lstAppUsers.ClearValues();
-
-    //    ListScrollerValue lstAppUserValue;
-    //    for (int i = 0; i < StateManager.Instance.IdentityFulls.Count; i++)
-    //    {
-    //        lstAppUserValue = new ListScrollerValue(4, true);
-    //        IdentityFull identityFull = StateManager.Instance.IdentityFulls[i];
-
-    //        lstAppUserValue.SetText(0, identityFull.OriginCountry);  //identityFull.BirthCity);
-    //        lstAppUserValue.SetText(1, $"{identityFull.Email}");
-    //        lstAppUserValue.SetSprite(2, sprEmpty);
-    //        lstAppUserValue.SetSprite(3, sprEmpty);
-
-    //        //if (identityFull.AppUserStatusId == 0)
-    //        //{
-    //        //    lstAppUserValue.SetText(0, identityFull.OriginCountry);  //identityFull.BirthCity);
-    //        //    lstAppUserValue.SetText(1, $"{identityFull.Email}");
-    //        //    lstAppUserValue.SetSprite(2, sprEmpty);
-    //        //    lstAppUserValue.SetSprite(3, sprEmpty);
-    //        //}
-    //        //else
-    //        //{
-    //        //    lstAppUserValue.SetText(0, identityFull.OriginCountry);  //identityFull.DpiCui);
-    //        //    lstAppUserValue.SetText(1, $"{identityFull.FirstNames} {identityFull.LastNames}");
-    //        //    lstAppUserValue.SetSprite(2, sprEmpty); //RM
-    //        //    lstAppUserValue.SetSprite(3, sprEmpty); //RM
-    //        //}
-
-    //        lstAppUsers.AddValue(lstAppUserValue);
-    //    }
-
-    //    lstAppUsers.ApplyValues();
-
-    //    Display(0);
-
-    //    StateManager.Instance.BoardLoadHide();
-    //}
 
     public void Display(int idx)
     {
