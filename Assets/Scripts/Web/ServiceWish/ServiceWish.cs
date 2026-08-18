7659bcd76332652d3e2c4c44614825aeb9742963
leapfortech <46363814+miralbes@users.noreply.github.com>
2026-08-18 16:32:52 -0600

    UPD Comment by Wish

--- Assets/Scripts/Web/ServiceWish/ServiceWish.cs
+++ Assets/Scripts/Web/ServiceWish/ServiceWish.cs
@@ -5,7 +5,7 @@ public class ServiceWish
     public long Id { get; set; }
     public long AppUserId { get; set; }
     public long ServiceTypeId { get; set; }
-    public String Comment { get; set; }
+    public String Wish { get; set; }
     public DateTime CreateDateTime { get; set; }
     public int Status { get; set; }
 
@@ -13,12 +13,12 @@ public class ServiceWish
     { 
     }
 
-    public ServiceWish(long id, long appUserId, long serviceTypeId, String comment, DateTime createDateTime, int status)
+    public ServiceWish(long id, long appUserId, long serviceTypeId, String wish, DateTime createDateTime, int status)
     {
         Id = id;
         AppUserId = appUserId;
         ServiceTypeId = serviceTypeId;
-        Comment = comment;
+        Wish = wish;
         CreateDateTime = createDateTime;
         Status = status;
     }
